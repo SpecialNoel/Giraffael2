d707bffca23928cacb2608ea244f8b0fdba4e787
JianmingLinUMass <jianminglin2893@gmail.com>
2024-12-31 21:28:45 -0500

    Replaced address instances in server.py with clientsocket.getpeername()

--- src/client.py
+++ src/client.py
@@ -4,7 +4,6 @@
 # Note: must be executed when the server is alive.
 
 import socket
-from datetime import datetime
 from threading import Thread
 
 
@@ -23,28 +22,37 @@ def init_client_socket(serverIP, serverPort):
 def recv_msg_from_channel(clientSocket):
     # Receive message from other clients in the channel
     while True:
+        if not is_connection_alive(clientSocket):
+            break
+        
         msg = clientSocket.recv(1024)
-        # Received a message with length 0 (only possible from server)
+        # Received a message of string 0 (only possible from server)
         # This means that the server wants to close the connection
         #   established by this client b/c of max num of clients reached
         if len(msg) == 0: break 
-        print('\n' + msg.decode())
+        print(msg.decode() + '\n')
 
     
-def get_and_send_user_input_msg(clientSocket, clientName):
-    print('\nType a message to send to the channel, or\nType q to disconnect.')
+def get_and_send_user_input_msg(clientSocket):
+    ruleSent = False
+
     while True:
+        if not ruleSent: 
+            ruleSent = True
+            print('\nType a message to send to the channel, ',
+                  'or\nType q to disconnect.\n')
+            
         msg = input()
+        send_msg_to_channel(clientSocket, msg)
         if msg.lower() == 'q':
-            print('\nDisconnected from the channel.')
+            print('Disconnected from the channel.\n')
             break
-        send_msg_to_channel(clientSocket, clientName, msg)
 
 
-def send_msg_to_channel(clientSocket, clientName, msg):
-    # Send message with current time to the channel
-    date_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    msg = f'[{date_now} {clientName}: {msg}]'
+def send_msg_to_channel(clientSocket, msg):
+    # Send message to the channel
+    if not is_connection_alive(clientSocket):
+        exit()
     clientSocket.send(msg.encode())
     
     
@@ -52,14 +60,24 @@ def check_if_init_connection_is_alive(clientSocket):
     msg = clientSocket.recv(1024)
     print(f'Init msg from server: {msg.decode()}.')
     
-    if msg.decode() == '1':
-        print('Connected to server successfully!')
-    elif msg.decode() == '0':
+    if msg.decode() == '-1':
         print('Connection refused by server: max client count reached.')
     else:
-        print('Unhandled msg sent from server.')
-        
-    return msg.decode() == '1'
+        print('Connected to server successfully!')
+    
+    return msg.decode() != '-1'
+
+
+def is_connection_alive(clientSocket):
+    try:
+        clientSocket.send(b'')
+        return True
+    except (BrokenPipeError, 
+            ConnectionResetError, 
+            ConnectionAbortedError) as e:
+        print(f'Error: {e}. ',
+              f'Connection with server is no longer alive.')
+        return False
 
 
 if __name__ == '__main__':
@@ -72,14 +90,14 @@ if __name__ == '__main__':
         exit()
         
     # Use thread t1 to receive message
-    clientName = input('Enter your username: ')
+    #clientName = input('Enter your username: ')
     t1 = Thread(target=recv_msg_from_channel, args=(clientSocket,))
     t1.daemon = True
     t1.start()
 
     # Use thread t2 to send message
     t2 = Thread(target=get_and_send_user_input_msg, 
-                args=(clientSocket,clientName,))
+                args=(clientSocket,))
     t2.daemon = True
     t2.start()
     
--- src/server.py
+++ src/server.py
@@ -6,6 +6,7 @@
 #   https://thepythoncode.com/article/make-a-chat-room-application-in-python
 
 import socket
+from datetime import datetime
 from threading import Thread
 
 
@@ -19,67 +20,126 @@ def init_server_socket(serverIP, serverPort):
     return serverSocket
 
 
-def handle_one_client(conn, address, clientSockets):
+def is_socket_alive(clientSocket):
+    try:
+        clientSocket.send(b'')
+        return True
+    except (BrokenPipeError, 
+            ConnectionResetError, 
+            ConnectionAbortedError) as e:
+        print(f'Error: {e}. ',
+              f'Connection with {clientSocket} is no longer alive.')
+        return False
+    
+    
+def handle_client_disconnect_request(conn, 
+                                     clientSockets, 
+                                     max_client_count):
+    print(f'Client on [{conn.getpeername()}] disconnected.')
+    conn.close()
+    clientSockets.remove(conn)
+    print(f'Connected clients: [{len(clientSockets)}/{max_client_count}]\n')
+
+
+def handle_one_client(conn, clientSockets, max_client_count):
     while True:
         try:
             # Receive message from one client
             msg = conn.recv(1024)
-        except Exception as e:
+            
+            # Disconnect from this connection if msg is only 'q'
+            decodedMsg = msg.decode()
+            print(f'Message from {conn.getpeername()}:', decodedMsg)
+            if decodedMsg.lower() == 'q':
+                handle_client_disconnect_request(conn, 
+                                                 clientSockets, 
+                                                 max_client_count)
+                break
+            
+            clientSocketsToBeRemoved = []
+            # Broadcast received message to all clients
+            for socket in clientSockets:
+                # Test if client socket is still alive before sending msg
+                # If not, remove it from client socket list
+                if not is_socket_alive(socket):
+                    clientSocketsToBeRemoved.append(socket)
+                    continue
+                
+                date_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                msgWithTime = f'[{date_now} {socket.conn.getpeername()}: '
+                msgWithTime += f'{decodedMsg}]'
+                socket.send(msgWithTime.encode())
+                
+            for socket in clientSocketsToBeRemoved:
+                clientSockets.remove(socket)
+        except (BrokenPipeError, 
+                ConnectionResetError, 
+                ConnectionAbortedError) as e:
             # Close connection with this client
+            print(f'Error: {e}. Removed {conn.getpeername()}', 
+                  ' from client socket list.')
             conn.close()
             clientSockets.remove(conn)
-            print(f'Error: {e}. Removed {address} from client socket list.')
-        else:
-            # Broadcast received message to all clients
-            print(f'Message from {address}:', msg.decode())
-            for socket in clientSockets:
-                socket.send(msg)
+            print(f'Connected clients: ',
+                  f'[{len(clientSockets)}/{max_client_count}]\n')
+            break
 
 
 def accept_a_connection(serverSocket, clientSockets, max_client_count):
     # Accept the connection
-    (conn, address) = serverSocket.accept()
+    try:
+        (conn, address) = serverSocket.accept()
+    except (KeyboardInterrupt) as e:
+        print(f'Error: {e}. Disconnected with all clients and exiting now.')
+        count = len(clientSockets)
+        for socket in clientSockets:
+            print(f'Removed {socket.getpeername()}', 
+                  'from client socket list.')
+            socket.close()
+            count -= 1
+            print(f'Connected clients: [{count}/{max_client_count}]\n')
+        exit()
     
     # Refuse the connection if reached max client count already
     if len(clientSockets) >= max_client_count:
         print(f'Max client count [{max_client_count}] reached.'
-             ,f'Refused connection from {address}')
-        conn.send(b'0') # refuse this connection by sending an string of 0
+             ,f'Refused connection from {address}.\n')
+        conn.send(b'-1') # refuse this connection by sending an string of -1
         conn.close()
         return
          
-    clientSockets.add(conn)
-    conn.send(b'1') # accept this connection by sending an string of 1
+    clientSockets.append(conn)
+    msg = str(len(clientSockets))
+    conn.send(msg.encode()) # accept connection by sending len(clientSockets)
     print(f'Accepted connection request from Client on [{address}].')
-    
+    print(f'Connected clients: [{len(clientSockets)}/{max_client_count}]\n')
+
     # Start a new thread that handles this client only
-    t = Thread(target=handle_one_client, args=(conn,address,clientSockets,))
-    t.daemon = True # Daemon thread to make it ends when the main thread ends
+    t = Thread(target=handle_one_client, args=(conn,
+                                               clientSockets,
+                                               max_client_count))
+    t.daemon = False # Set daemon thread: ends when the main thread ends
     t.start()
          
         
 if __name__=='__main__':
-    MAX_CLIENT_COUNT = 1
+    MAX_CLIENT_COUNT = 2
     SERVER_IP = '127.0.0.1'
     SERVER_PORT = 5001
     
     serverSocket = init_server_socket(SERVER_IP, SERVER_PORT)
-    clientSockets = set()
+    clientSockets = []
     
     # Start listening for connection
     serverSocket.listen(MAX_CLIENT_COUNT)
     print(f'Server socket on [{SERVER_IP}: {SERVER_PORT}] started listening.')
-    print(f'MAX {MAX_CLIENT_COUNT} Clients.\n')
+    print(f'MAX: {MAX_CLIENT_COUNT} clients.')
+    print(f'Connected clients: [{len(clientSockets)}/{MAX_CLIENT_COUNT}]\n')
         
     while True:
         # Start accepting connections established by clients
         accept_a_connection(serverSocket, clientSockets, MAX_CLIENT_COUNT)
-        
-        # If the last connected client disconnects, stop accepting more.
-        if not len(clientSockets):
-            print('No more Client connected to the channel. Channel closed.')
-            break
-    
+            
     serverSocket.close()
     print('Server socket closed.')
     
