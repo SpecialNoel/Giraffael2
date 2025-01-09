82dcc988a3ccb8e08ca393eeb4f23ed5a534ac79
JianmingLinUMass <jianminglin2893@gmail.com>
2025-01-09 17:34:55 -0500

    Cleaned up server part, and refactored client part into a class.

--- src/client.py
+++ src/client.py
@@ -11,157 +11,164 @@ import socket
 from threading import Thread, Event
 
 
-def init_client_socket(serverIP, serverPort):
-    try: 
-        # TCP connection
-        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        # Connect to the server socket with the [IP, Port number] combination
-        client.connect((serverIP, serverPort)) 
-        return client
-    except ConnectionRefusedError:
-        print('Connection refused. Server is not on yet.')
-        return None
+class Client:
+    def __init__(self):
+        self.SERVER_IP = '127.0.0.1'
+        self.SERVER_PORT = 5001
+        
+        self.client = self.init_client_socket() # client socket
+        
+        self.shutdownEvent = Event() # threading.Event()
 
 
-def recv_msg_from_channel(client, shutdownEvent):
-    # Receive message from other clients in the channel
-    while not shutdownEvent.is_set():
-        try:
-            msg = client.recv(1024)
-            # Received a message of string 0 (only possible from server)
-            # This means that the server wants to close the connection
-            #   established by this client b/c of max num of clients reached
-            if not msg:
-                print('Server has closed the connection.')
-                shutdownEvent.set()
+    def init_client_socket(self):
+        try: 
+            # TCP connection
+            self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            # Connect to the server socket with [IP, Port number] combination
+            self.client.connect((self.SERVER_IP, self.SERVER_PORT)) 
+            return self.client
+        except ConnectionRefusedError:
+            print('Connection refused. Server is not on yet.')
+            return None
+
+
+    def recv_msg_from_channel(self):
+        # Receive message from other clients in the channel
+        while not self.shutdownEvent.is_set():
+            try:
+                msg = self.client.recv(1024)
+                # Received a message of string 0 (only possible from server)
+                # This means that the server wants to close this connection
+                #   b/c of max clients count reached in server
+                if not msg:
+                    print('Server has closed the connection.')
+                    self.shutdownEvent.set()
+                    break
+                print(msg.decode() + '\n')
+            except Exception as e:
+                print(f'Error [{e}] occured when receiving message.')
                 break
-            print(msg.decode() + '\n')
-        except Exception as e:
-            print(f'Error [{e}] occured when receiving message.')
-            break
-    client.close()
-    print('Client receiver thread stopped.')
-    return
+        self.client.close()
+        print('Client receiver thread stopped.')
+        return
 
-    
-def get_and_send_user_input_msg(client, shutdownEvent):
-    ruleSent = False
+        
+    def get_and_send_user_input_msg(self):
+        ruleSent = False
 
-    while not shutdownEvent.is_set():
-        if not ruleSent: 
-            ruleSent = True
-            print('\nType a message to send to the channel, ',
-                  'or\nPress [Enter/Return] key to disconnect.\n')
-            
+        while not self.shutdownEvent.is_set():
+            if not ruleSent: 
+                ruleSent = True
+                print('\nType a message to send to the channel, ',
+                    'or\nPress [Enter/Return] key to disconnect.\n')
+                
+            msg = input()
+            if msg == '':
+                print('Disconnected from the channel.\n')
+                self.shutdownEvent.set()
+                self.client.close()
+                break
+            else:
+                self.client.send(msg.encode())
+        print('Client sender thread stopped.')
+        return
+
+
+    def recv_user_input_and_send_to_server(self):
         msg = input()
-        if msg == '':
-            print('Disconnected from the channel.\n')
-            shutdownEvent.set()
-            client.close()
-            break
+        self.client.send(msg.encode())
+        response = self.client.recv(1024)
+        return msg, response.decode()
+
+        
+    def send_username_to_server(self):
+        print('Type in your username: ')
+        return self.recv_user_input_and_send_to_server()
+        
+
+    def send_decision_on_room_to_server(self):
+        print('Type in a single letter <C> to Create a new room, OR')
+        print('Type in a single letter <E> to Enter an existing room.')
+        return self.recv_user_input_and_send_to_server()
+
+
+    def send_room_code_to_server(self):
+        print('Type in the room code to enter an existing room.')
+        return self.recv_user_input_and_send_to_server()
+
+        
+    def check_if_server_reached_max_client_capacity(self):
+        msg = self.client.recv(1024)
+        print(f'Init msg from server: {msg.decode()}.')
+        
+        if msg.decode() == '-1':
+            print('Connection refused by server: max client count reached.')
         else:
-            client.send(msg.encode())
-    print('Client sender thread stopped.')
-    return
+            print('Connected to server successfully!')
+        
+        return msg.decode() != '-1'
 
+
+    def is_connection_alive(self):
+        try:
+            self.client.send(b'')
+            return True
+        except (BrokenPipeError, 
+                ConnectionResetError, 
+                ConnectionAbortedError) as e:
+            print(f'Error: {e}. ',
+                  f'Connection with server is no longer alive.')
+            return False
+        
+        
+    def run_client(self):
+        if self.client == None:
+            exit()
+            
+        if not self.check_if_server_reached_max_client_capacity():
+            exit()
+            
+        # If msg is 'C', response will be a room code generated by server
+        # If msg is 'E', response will be:
+        #   1. 'VALID_ROOM_CODE' sent by server, if the room code is valid
+        #   2. an error message sent by server, otherwise
+        # Otherwise,     response will be an error message sent by server
+        msg, responseOnRoomCode = self.send_decision_on_room_to_server()
+        while msg.upper() != 'C':
+            print(f'msg: {msg}, response from server: {responseOnRoomCode}')
+            if msg.upper() == 'E':
+                msg, responseOnRoomCode = self.send_room_code_to_server()
+                if msg.upper() == 'C':
+                    break
+                if responseOnRoomCode == 'VALID_ROOM_CODE':
+                    responseOnRoomCode = msg
+                    break
+            msg, responseOnRoomCode = self.send_decision_on_room_to_server()
+                
+        msg, responseOnUsername = self.send_username_to_server()
+        while responseOnUsername != 'VALID_USERNAME':
+            print(responseOnUsername)
+            msg, responseOnUsername = self.send_username_to_server()
+            
+        # Use thread t1 to receive message
+        t1 = Thread(target=self.recv_msg_from_channel, args=())
+        t1.daemon = True
+        t1.start()
+
+        # Use thread t2 to send message
+        t2 = Thread(target=self.get_and_send_user_input_msg, args=())
+        t2.daemon = True
+        t2.start()
     
-def send_username_to_server(client):
-    print('Type in your username: ')
-    msg = input()
-    client.send(msg.encode())
-    response = client.recv(1024)
-    return response.decode()
-    
+        t1.join()
+        t2.join()
 
-def send_decision_on_room_to_server(client):
-    print('Type in a single letter <C> to Create a new room, OR')
-    print('Type in a single letter <E> to Enter an existing room.')
-    msg = input()
-    client.send(msg.encode())
-    response = client.recv(1024)
-    return msg, response.decode()
-
-
-def send_room_code_to_server(client):
-    print('Type in the room code to enter an existing room.')
-    msg = input()
-    client.send(msg.encode())
-    response = client.recv(1024)
-    return msg, response.decode()
-
-    
-def check_if_server_reached_max_client_capacity(client):
-    msg = client.recv(1024)
-    print(f'Init msg from server: {msg.decode()}.')
-    
-    if msg.decode() == '-1':
-        print('Connection refused by server: max client count reached.')
-    else:
-        print('Connected to server successfully!')
-    
-    return msg.decode() != '-1'
-
-
-def is_connection_alive(client):
-    try:
-        client.send(b'')
-        return True
-    except (BrokenPipeError, 
-            ConnectionResetError, 
-            ConnectionAbortedError) as e:
-        print(f'Error: {e}. ',
-              f'Connection with server is no longer alive.')
-        return False
-
+        print('Client socket closed.')
+        exit()
+        
 
 if __name__ == '__main__':
-    SERVER_HOST = '127.0.0.1'
-    SERVER_PORT = 5001
+    client = Client()
+    client.run_client()
     
-    client = init_client_socket(SERVER_HOST, SERVER_PORT)
-    if client == None:
-        exit()
-    
-    if not check_if_server_reached_max_client_capacity(client):
-        exit()
-            
-    # If msg is 'C', responseOnRoomCode will be a room code generated by server
-    # If msg is 'E', responseOnRoomCode will be:
-    #   1. 'VALID_ROOM_CODE' sent by server, if room code from client is valid
-    #   2. an error message sent by server, otherwise
-    # Otherwise,     responseOnRoomCode will be an error message sent by server
-    msg, responseOnRoomCode = send_decision_on_room_to_server(client)
-    while msg.upper() != 'C':
-        print(f'msg: {msg}, response from server: {responseOnRoomCode}')
-        if msg.upper() == 'E':
-            msg, responseOnRoomCode = send_room_code_to_server(client)
-            if msg.upper() == 'C':
-                break
-            if responseOnRoomCode == 'VALID_ROOM_CODE':
-                responseOnRoomCode = msg
-                break
-        msg, responseOnRoomCode = send_decision_on_room_to_server(client)
-            
-    responseOnUsername = send_username_to_server(client)
-    while responseOnUsername != 'VALID_USERNAME':
-        print(responseOnUsername)
-        responseOnUsername = send_username_to_server(client)
-        
-    shutdownEvent = Event() # threading.Event()
-
-    # Use thread t1 to receive message
-    t1 = Thread(target=recv_msg_from_channel, args=(client,shutdownEvent,))
-    t1.daemon = True
-    t1.start()
-
-    # Use thread t2 to send message
-    t2 = Thread(target=get_and_send_user_input_msg, 
-                args=(client,shutdownEvent,))
-    t2.daemon = True
-    t2.start()
-    
-    t1.join()
-    t2.join()
-
-    #client.close()
-    print('Client socket closed.')
