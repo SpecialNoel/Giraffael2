a5bc433a075f995cc1db16d202865d75336c515f
JianmingLinUMass <jianminglin2893@gmail.com>
2025-01-08 23:36:51 -0500

    WIP: refactoring server to a class.

---
+++ src/chat_app.py
@@ -0,0 +1,11 @@
+# To run this script: python3 chat_app.py
+
+from server import Server
+
+class Chat_App:
+    def __init__():
+        server = Server()
+        server.run_server()
+    
+if __name__=='__main__':  
+    Chat_App()
