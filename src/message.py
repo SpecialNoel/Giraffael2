61da882123762c22ff35faca14fd0fc78382a985
JianmingLinUMass <jianminglin2893@gmail.com>
2025-01-11 14:16:55 -0500

    Fixed the issue where clients can press [Enter] when answering questions sent by server (asking for room code and username, for example), which will stuck both server and client sides.

--- src/message.py
+++ src/message.py
@@ -1,4 +1,12 @@
 # message.py
 
-def strip_message(message): # message is a string
-    return message.strip()
+# Functions here are used to argument messages transmitted
+#   between clients and server.
+
+# Return a copy of the string with trailing spaces or new line chars removed.
+# Mainly used for removing the new line character at the end of the message.
+# If message is empty, return the original message.
+def rstrip_message(message): # message is a string
+    if not message:
+        return message
+    return message.rstrip()
