9232ce0221a144fe98dea39f046d2613d4323557
JianmingLinUMass <jianminglin2893@gmail.com>
2025-02-01 18:24:34 -0500

    Added code for handling api key in remote server.

--- src/server_only/retrieve_secret_from_aws.py
+++ src/server_only/retrieve_secret_from_aws.py
@@ -5,7 +5,19 @@ import ssl
 from botocore.exceptions import ClientError
 from io import BytesIO
 
-def get_secret():
+def get_secret_api_key():
+    # Initialize SSM client
+    region_name = "us-east-2"
+    ssm = boto3.client('ssm', region_name=region_name)
+
+    # Fetch the parameter
+    response = ssm.get_parameter(Name='/Giraffael2/OPENAI_API_KEY', WithDecryption=True)
+
+    # Extract the key
+    apiKey = response['Parameter']['Value']
+    return apiKey
+
+def get_secret_cert_and_key():
     secret_name = "Secret-for-Giraffael-2"
     region_name = "us-east-2"
 
@@ -32,7 +44,7 @@ def get_secret():
     
 def setup_ssl_context():
     context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER) # TLS
-    certification, privateKey = get_secret()
+    certification, privateKey = get_secret_cert_and_key()
     context.load_cert_chain(certfile=BytesIO(certification.encode()), 
                             keyfile=BytesIO(privateKey.encode()))
     print('SSL context on server side loaded successfully!')
