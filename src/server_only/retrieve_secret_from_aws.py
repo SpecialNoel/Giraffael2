c107bc8d71afe47952a4668dcdd276244c915dc2
JianmingLinUMass <jianminglin2893@gmail.com>
2025-02-02 14:58:59 -0500

    Cleaned up code in retrieve_secret_from_aws.py

--- src/server_only/retrieve_secret_from_aws.py
+++ src/server_only/retrieve_secret_from_aws.py
@@ -4,67 +4,30 @@ import boto3
 import json
 import ssl
 import tempfile
-from botocore.exceptions import ClientError
-
-def get_api_key():
-    secret_name = "Secret-for-Giraffael-2"
-    region_name = "us-east-2"
-
-    # Create a Secrets Manager client
-    session = boto3.session.Session()
-    client = session.client(
-        service_name='secretsmanager',
-        region_name=region_name
-    )
-
-    try:
-        get_secret_value_response = client.get_secret_value(
-            SecretId=secret_name
-        )
-    except ClientError as e:
-        # For a list of exceptions thrown, see
-        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-        raise e
-
-    secretJson = get_secret_value_response['SecretString']
-    secret = json.loads(secretJson)
-    apiKey = secret['openai_api_key']
-    return apiKey
 
 def get_secret():
     secret_name = "Secret-for-Giraffael-2"
     region_name = "us-east-2"
+    client = boto3.client("secretsmanager", region_name)
+    response = client.get_secret_value(SecretId=secret_name)
+    return json.loads(response["SecretString"])
 
-    # Create a Secrets Manager client
-    session = boto3.session.Session()
-    client = session.client(
-        service_name='secretsmanager',
-        region_name=region_name
-    )
+def get_api_key():
+    secret = get_secret()
+    return secret['openai_api_key']
 
-    try:
-        get_secret_value_response = client.get_secret_value(
-            SecretId=secret_name
-        )
-    except ClientError as e:
-        # For a list of exceptions thrown, see
-        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-        raise e
-
-    secretJson = get_secret_value_response['SecretString']
-    secret = json.loads(secretJson)
-    certification = secret['cert.pem']
-    privateKey = secret['key.pem']
-    return certification, privateKey
+def get_cert_and_key():
+    secret = get_secret()
+    return secret['cert.pem'], secret['key.pem']
     
 def setup_ssl_context():
     context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER) # TLS
-    certification, privateKey = get_secret()
+    certificate, privateKey = get_cert_and_key()
     
     with (tempfile.NamedTemporaryFile(delete=True) as cert_file, 
           tempfile.NamedTemporaryFile(delete=True) as key_file):
         # Write certificate and key to the temporary files
-        cert_file.write(certification.encode())
+        cert_file.write(certificate.encode())
         cert_file.flush()
         key_file.write(privateKey.encode())
         key_file.flush()
