eece1b46853c23b2fe5dde55cf299408aa0a63f8
JianmingLinUMass <jianminglin2893@gmail.com>
2025-02-01 19:23:27 -0500

    Moved api key to another location.

--- src/server_only/retrieve_secret_from_aws.py
+++ src/server_only/retrieve_secret_from_aws.py
@@ -6,19 +6,32 @@ import ssl
 import tempfile
 from botocore.exceptions import ClientError
 
-def get_secret_api_key():
-    # Initialize SSM client
+def get_api_key():
+    secret_name = "Secret-for-Giraffael-2"
     region_name = "us-east-2"
-    ssm = boto3.client('ssm', region_name=region_name)
 
-    # Fetch the parameter
-    response = ssm.get_parameter(Name='/Giraffael2/OPENAI_API_KEY', WithDecryption=True)
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
 
-    # Extract the key
-    apiKey = response['Parameter']['Value']
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+
+    secretJson = get_secret_value_response['SecretString']
+    secret = json.loads(secretJson)
+    apiKey = secret['openai_api_key']
     return apiKey
 
-def get_secret_cert_and_key():
+def get_secret():
     secret_name = "Secret-for-Giraffael-2"
     region_name = "us-east-2"
 
@@ -42,13 +55,11 @@ def get_secret_cert_and_key():
     secret = json.loads(secretJson)
     certification = secret['cert.pem']
     privateKey = secret['key.pem']
-    print("Certificate snippet:", certification[:10])
-    print("PrivateKey snippet:", privateKey[:10])
     return certification, privateKey
     
 def setup_ssl_context():
     context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER) # TLS
-    certification, privateKey = get_secret_cert_and_key()
+    certification, privateKey = get_secret()
     
     with (tempfile.NamedTemporaryFile(delete=True) as cert_file, 
           tempfile.NamedTemporaryFile(delete=True) as key_file):
