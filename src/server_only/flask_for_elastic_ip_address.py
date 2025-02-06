962cbb90a9802db8c503ea884fa83b46eb7b5945
JianmingLinUMass <jianminglin2893@gmail.com>
2025-02-06 17:52:20 -0500

    Added flask_for_elastic_ip_address.py.

---
+++ src/server_only/flask_for_elastic_ip_address.py
@@ -0,0 +1,26 @@
+# flask_for_elastic_ip_address.py
+
+import boto3
+import os
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+ssm = boto3.client('ssm',
+                   region_name=os.getenv('AWS_REGION', 'us-east-1'))
+
+@app.route('/get-server-ip', methods=['GET'])
+def get_server_ip():
+    try:
+        # Fetch server's elastic ip address from Parameter Store
+        parameterName = '/my-app/server-elastic-ip'
+        response = ssm.get_parameter(Name=parameterName, WithDecryption=False)
+        elasticIp = response['Parameter']['Value']
+        
+        # Return the Elastic IP as a JSON response
+        return jsonify({"Elastic ip": elasticIp}), 200
+    except Exception as e:
+        return jsonify({"Error": str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
