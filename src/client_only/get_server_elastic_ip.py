4adb110cc615add88cbbdd7cb62d8c01a66614c2
JianmingLinUMass <jianminglin2893@gmail.com>
2025-02-06 20:10:21 -0500

    Modified client side function.

--- src/client_only/get_server_elastic_ip.py
+++ src/client_only/get_server_elastic_ip.py
@@ -1,12 +1,13 @@
 # get_server_elastic_ip.py
 
-import boto3
+import requests
 
 def get_server_elastic_ip():
-    ssm = boto3.client('ssm', region_name='us-east-2')
-
-    response = ssm.get_parameter(Name='Giraffael2-server-ip-address')
-    elasticIp = response['Parameter']['Value']
-
-    print('Elastic Ip:', elasticIp)
-    return elasticIp
+    response = requests.get('http://Giraffael2-Load-Balancer-1365680863.us-east-2.elb.amazonaws.com/get-server-ip')
+    if response.status_code == 200:
+        elasticIp = response.json()['ElasticIp']
+        print(f"Elastic IP: {elasticIp}")
+        return elasticIp
+    else:
+        print("Failed to fetch the Elastic IP:", response.json())
+        return ''
