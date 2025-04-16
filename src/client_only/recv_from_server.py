535c17f4b370efbbef525e2cfc27725b416d2d70
JianmingLinUMass <jianminglin2893@gmail.com>
2025-04-16 10:04:59 -0400

    Working on the issue on base-project branch

--- src/client_only/recv_from_server.py
+++ src/client_only/recv_from_server.py
@@ -11,7 +11,7 @@ from client_only.handle_msg_suggestion import handle_msg_suggestion
 from general.file_transmission import *
 from general.message import (get_prefix_and_content, rstrip_message, 
                              recv_decoded_content)
-
+ 
 ''' 
 def handle_recv_request_with_match(prefix, typePrefix, msgContent, client, 
                                    chunkSize, maxFileSize, extList):
