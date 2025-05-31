59a871b715a23c80bc7535b5ea2f50350dc74348
SpecialNoel <jianminglin2893@gmail.com>
2025-05-30 22:36:32 -0400

    Added two ways to achieve client send and fetch operations.

--- v3-src/client/encryption.py
+++ v3-src/client/encryption.py
@@ -3,12 +3,14 @@
 import os
 from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 
+# Encrypt a plain text with AESGCM and a 12 bytes random nonce
 def encrypt(key: bytes, plainText: bytes) -> dict:
     aesgcm = AESGCM(key)
     nonce = os.urandom(12)
     cipherText = aesgcm.encrypt(nonce, plainText, None)
     return {'cipherText': cipherText, 'nonce': nonce}
 
+# Decrypt a cipher text with AESGCM and the same nonce used for the encryption
 def decrypt(key: bytes, cipherText: bytes, nonce: bytes) -> bytes:
     aesgcm = AESGCM(key)
     return aesgcm.decrypt(nonce, cipherText, None) # plainText: bytes
--- v3-src/utilities/requirements.sh
+++ v3-src/utilities/requirements.sh
@@ -2,5 +2,6 @@
 
 # To run this script: ./v3-src/utilities/requirements.sh 
 
-pip install fastapi # for server front-end operations
-pip install uvicorn # for hosting server with given IP and port number
+pip install fastapi    # for server front-end operations
+pip install uvicorn    # for hosting server with given IP and port number
+pip install websockets # for client utilizing websockets
