5dc8452bfec382e8ecefa73b36a308d4ac0b06bb
SpecialNoel <jianminglin2893@gmail.com>
2026-01-14 21:54:34 -0500

    Deleted creating-file related scripts and requirements. Also deleted encryption related files

--- src/utilities/requirements.sh
+++ src/utilities/requirements.sh
@@ -1,14 +1,8 @@
 # requirements.sh
 
 # To run this script: ./utilities/requirements.sh 
-# Note: Have to cd to the src folder for this to work
 
-pip install openpyxl   # for create_excel.py
-pip install pillow     # for create_img.py
-pip install reportlab  # for create_pdf.py
-pip install openai     # for generate_msg_suggestion.py
-pip install python-dotenv # for OpenAI API key
-pip install boto3      # for accessing AWS Secret Manager
-pip install pycryptodome cryptography # for E2EE
-pip install Crypto     # for E2EE
-pip install pymongo    # for MongoDB
+pip install openai           # for message suggestion generation with OpenAI
+pip install python-dotenv    # for OpenAI API key
+pip install boto3            # for accessing AWS Secret Manager
+pip install pymongo          # for accessing MongoDB
