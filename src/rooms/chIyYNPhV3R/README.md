301348d39791f6920aeae1ac70c53bd72ff3c969
SpecialNoel <jianminglin2893@gmail.com>
2026-01-14 23:10:43 -0500

    Updated gitignore, and converted requirements to txt file

--- src/rooms/chIyYNPhV3R/README.md
+++
@@ -1,8 +0,0 @@
-# :giraffe: Giraffael :giraffe:
-
-Checkout Giraffael on [this Notion page](https://www.notion.so/Project-Giraffael-15fa829a7ea780a7a2cdf502792b7805).
-
-## Instruction:
-
-1. Run server side: python -m server_only.server_core.server
-2. Run client side: python -m client_only.client_core.client
--- utilities/requirements.sh
+++
@@ -1,9 +0,0 @@
-# requirements.sh
-
-# ./utilities/requirements.sh 
-
-pip install openai           # for message suggestion generation with OpenAI
-pip install python-dotenv    # for accessing environmental variables
-pip install boto3            # for accessing AWS Secret Manager
-pip install pymongo          # for accessing MongoDB
-pip install requests         # for accessing AWS EC2 instance elastic ip
